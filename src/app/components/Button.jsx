3a325cf74ff6e790f9ecb1851b0d80328bb6cb93
Nahuel Benitez <jorgenahuelbenitez07@gmail.com>
2023-10-24 19:54:13 -0300

    create generic input and button in components

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-
-
 function Button(props) {
   return (
     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  w-full">
       {props.text}
-    </button>
-    
+    </button>    
   )
 }
 
--- src/app/components/Input.jsx
+++ src/app/components/Input.jsx
@@ -1,4 +1,3 @@
-
 function Input(props) {
   return (
     <input className="bg-gray-200 rounded-lg py-2 px-4 block w-full sm:w-auto sm:w-full" type="text" placeholder={props.placeholder} />

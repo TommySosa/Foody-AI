b6f0ff1dd8ec14bee88ee9b3fd79e285bd734a03
TommySosa <tomas.facundo.sosa@gmail.com>
2023-10-23 21:53:22 -0300

    first commit

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { useState } from "react";
+
+export default function Header() {
+    const Links = [
+        { name: "HOME", link: "/" },
+        { name: "CREATE YOUR PLAN", link: "/" },
+    ];
+    const [open, setOpen] = useState(false);
+
+    return (
+        <header className='shadow-md w-full fixed top-0 left-0'>
+            <div className='md:flex items-center  bg-primary  py-4 md:px-10 px-7'>
+                <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
+                    <span>FoodyAI</span>
+                </div>
+                <div onClick={() => setOpen(!open)} className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'>
+                    {
+                        open ? <>X</> : <>☰</>
+                    }
+                </div>
+                <ul className={`md:flex md:items-center bg-primary md:pb-0 pb-2 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
+                    {
+                        Links.map((link) => (
+                            <li className='md:ml-8 md:my-0 my-7 font-semibold' key={link.name}>
+                                <a href={link.link} className='text-gray-800 hover:text-white duration-500'>{link.name}</a>
+                            </li>))
+                    }
+                </ul>
+            </div>
+        </header>
+    );
+}

2f7d67e9cd3afa52cbcc0d396db0ece2246d028d
Nahuel Benitez <jorgenahuelbenitez07@gmail.com>
2023-10-24 19:36:50 -0300

    add props in a generic button

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+
 function Button(props) {
   return (
     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  w-full">
       {props.text}
     </button>
+    
   )
 }
 

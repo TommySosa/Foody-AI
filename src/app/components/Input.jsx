3720c857debcb7249cb9beebe8ba0c9e764484db
Nahuel Benitez <jorgenahuelbenitez07@gmail.com>
2023-10-24 19:34:20 -0300

    create generic input and button

---
+++ src/app/components/Input.jsx
@@ -0,0 +1,8 @@
+
+function Input(props) {
+  return (
+    <input className="bg-gray-200 rounded-lg py-2 px-4 block w-full sm:w-auto sm:w-full" type="text" placeholder={props.placeholder} />
+  )
+}
+
+export default Input
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,11 @@
+import Button from "./components/Button";
+import Input from './components/Input';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       HOME
+      <Button text="Hola" />
+      <Input placeholder="Ejemplo"/>
     </main>
   )
 }

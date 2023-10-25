33a72576b3e444b1ddf4f832cb7ad675d1a07cb4
Nahuel Benitez <jorgenahuelbenitez07@gmail.com>
2023-10-25 10:45:12 -0300

    config witdh button

--- src/app/components/Input.jsx
+++ src/app/components/Input.jsx
@@ -1,6 +1,6 @@
 function Input(props) {
   return (
-    <input className="bg-gray-200 rounded-lg py-2 px-4 block w-full sm:w-auto sm:w-full" type="text" placeholder={props.placeholder} />
+    <input className="bg-gray-200 rounded-lg py-2 px-4 block w-full  sm:w-full" type="text" placeholder={props.placeholder} />
   )
 }
 
